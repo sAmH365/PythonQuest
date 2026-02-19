b1690227e8b8103da899cc3d996e3268d601c161
sAmH365 <npower5377@naver.com>
2026-02-19 09:25:54 +0900

    피보나치

--- 섹션2/11_재귀.py
+++ 섹션2/11_재귀.py
@@ -17,3 +17,23 @@ def countdown(n):
   return text
 
 print(countdown(4))
+
+print('\n피보나치====================')
+my_fibo_dict = {}
+def fibo(n):
+  if n <= 0:
+    return None
+
+  if n == 1 or n == 2:
+    return 1
+
+  if n in my_fibo_dict:
+    return my_fibo_dict[n]
+
+  cur_n = fibo(n - 1) + fibo(n - 2)
+
+  my_fibo_dict[n] = cur_n
+
+  return cur_n
+
+print(fibo(100))
