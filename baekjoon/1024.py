a3a919298d6c1a4b50a2a0755827536a95716845
sAmH365 <npower5377@naver.com>
2026-02-25 11:06:17 +0900

    update baekjoon

--- baekjoon/1024.py
+++ baekjoon/1024.py
@@ -3,35 +3,59 @@ import sys
 
 input = sys.stdin.readline
 
+# N, L = list(map(int, input().split()))
+#
+# def check_arr_length(arr):
+#   return L <= len(arr) < 100
+#
+# result = set()
+# def find_num_arr():
+#   for i in range(N + 1):
+#     sum = i
+#     num_arr = [i]
+#     for j in range(i + 1, N + 1):
+#       if sum == N:
+#         result.add(tuple(num_arr))
+#       elif sum > N:
+#         break
+#       else:
+#         sum += j
+#         num_arr.append(j)
+#
+# find_num_arr()
+#
+#
+# sorted_list = sorted(result, reverse=True)
+# target_arr = [v for v in sorted_list if L <= len(v) < 100]
+#
+#
+# print(target_arr)
+#
+# if len(target_arr) == 0:
+#   print(-1)
+# else:
+#   print(*target_arr[0])
+
+
+# 연속된 숫자들 세팅 후 판별하기
 N, L = list(map(int, input().split()))
-
-def check_arr_length(arr):
-  return L <= len(arr) < 100
-
 result = set()
-def find_num_arr():
-  for i in range(N + 1):
-    sum = i
-    num_arr = [i]
-    for j in range(i + 1, N + 1):
-      if sum == N:
-        result.add(tuple(num_arr))
-      elif sum > N:
-        break
-      else:
-        sum += j
-        num_arr.append(j)
+def solve():
+  for l in range(L, 101):
+    tmp_sum = l * (l - 1) // 2 # 0 ~ l-1까지의 합, 등차수열 합공식
 
-find_num_arr()
+    remain = N - tmp_sum # 15
 
+    if remain % l == 0:
+      start = remain // l
+      end = start + l
 
-sorted_list = sorted(result, reverse=True)
-target_arr = [v for v in sorted_list if L <= len(v) < 100]
-
-
-print(target_arr)
-
-if len(target_arr) == 0:
+      if start >= 0:
+        for i in range(start, end):
+          print(i, end=' ')
+        return
   print(-1)
-else:
-  print(*target_arr[0])
+
+
+solve()
+# print(*result)
