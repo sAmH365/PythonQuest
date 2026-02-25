5586afb4e7a0fe50b39212eeef1aa1fd034bf206
sAmH365 <npower5377@naver.com>
2026-02-25 16:43:44 +0900

    병합정렬

--- 섹션5/정렬.py
+++ 섹션5/정렬.py
@@ -4,19 +4,57 @@ test_arr = random.sample(range(1, 51), 20)
 
 print(test_arr)
 
-def insertion_sort(arr):
+# def insertion_sort(arr):
+#
+#   for i in range(1, len(arr)):
+#     target = arr[i]
+#     j = i - 1
+#
+#     while j >= 0 and arr[j] > target:
+#       arr[j + 1] = arr[j]
+#       j -= 1
+#
+#     arr[j + 1] = target
+#
+#   return arr
+#
+#
+# print(insertion_sort(test_arr))
 
-  for i in range(1, len(arr)):
-    target = arr[i]
-    j = i - 1
 
-    while j >= 0 and arr[j] > target:
-      arr[j + 1] = arr[j]
-      j -= 1
+def merge_sort(arr, left, right) -> list:
+  if left >= right:
+    return arr
 
-    arr[j + 1] = target
+  mid = (left + right) // 2
+  merge_sort(arr, left, mid)
+  merge_sort(arr, mid + 1, right)
+  merge(arr, left, mid, right)
 
   return arr
 
+def merge(arr, left, mid, right):
+  tmp = []
+  i = left
+  j= mid + 1
 
-print(insertion_sort(test_arr))
+  while i <= mid and j <= right:
+    if arr[i] <= arr[j]:
+      tmp.append(arr[i])
+      i += 1
+    else:
+      tmp.append(arr[j])
+      j += 1
+
+  while i <= mid:
+    tmp.append(arr[i])
+    i += 1
+  while j <= right:
+    tmp.append(arr[j])
+    j += 1
+
+  for t in range(len(tmp)):
+    arr[left + t] = tmp[t]
+
+  return arr
+print(*merge_sort(test_arr, 0, len(test_arr) - 1))
