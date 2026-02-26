d66a004abbe02e02b0976c041b37f66ca76a08de
sAmH365 <npower5377@naver.com>
2026-02-26 09:29:34 +0900

    퀵정렬

--- 섹션5/정렬.py
+++ 섹션5/정렬.py
@@ -1,27 +1,28 @@
 import random
 
-test_arr = random.sample(range(1, 51), 20)
+print('=================삽입정렬==================')
+test_arr1 = random.sample(range(1, 51), 20)
+print(test_arr1)
+def insertion_sort(arr):
 
-print(test_arr)
+  for i in range(1, len(arr)):
+    target = arr[i]
+    j = i - 1
 
-# def insertion_sort(arr):
-#
-#   for i in range(1, len(arr)):
-#     target = arr[i]
-#     j = i - 1
-#
-#     while j >= 0 and arr[j] > target:
-#       arr[j + 1] = arr[j]
-#       j -= 1
-#
-#     arr[j + 1] = target
-#
-#   return arr
-#
-#
-# print(insertion_sort(test_arr))
+    while j >= 0 and arr[j] > target:
+      arr[j + 1] = arr[j]
+      j -= 1
+
+    arr[j + 1] = target
+
+  return arr
 
 
+print(insertion_sort(test_arr1))
+
+print('\n=================병합정렬==================')
+test_arr2 = random.sample(range(1, 51), 20)
+print(test_arr2)
 def merge_sort(arr, left, right) -> list:
   if left >= right:
     return arr
@@ -57,4 +58,29 @@ def merge(arr, left, mid, right):
     arr[left + t] = tmp[t]
 
   return arr
-print(*merge_sort(test_arr, 0, len(test_arr) - 1))
+print(*merge_sort(test_arr2, 0, len(test_arr2) - 1))
+
+print('\n=================퀵정렬==================')
+test_arr3 = random.sample(range(1, 51), 20)
+print(test_arr3)
+
+def quick_sort(arr, left, right):
+  if left < right:
+    pi = partition(arr, left, right)
+    quick_sort(arr, left, pi - 1)
+    quick_sort(arr, pi + 1, right)
+
+  return arr
+
+def partition(arr, left, right):
+  pivot = arr[right]
+  i = left - 1
+  for j in range(left, right):
+    if arr[j] < pivot:
+      i += 1
+      arr[i], arr[j] = arr[j], arr[i]
+  arr[i + 1], arr[right] = arr[right], arr[i + 1]
+
+  return i + 1
+
+print(*quick_sort(test_arr3, 0, len(test_arr3) - 1))
